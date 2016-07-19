36b1227478f8a9f850e3cfa6722a6acd123aaa39
ademphlvn <adem.phlvn@hotmail.com>
2016-07-19 16:34:34 +0300

    OkulAdoNet

---
+++ wfOKULAdoNet/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace wfOKULAdoNet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbSiniflar = new System.Windows.Forms.ComboBox();
+            this.lvOgrenciler = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(296, 59);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Sınıf Seçiniz";
+            // 
+            // cbSiniflar
+            // 
+            this.cbSiniflar.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbSiniflar.FormattingEnabled = true;
+            this.cbSiniflar.Location = new System.Drawing.Point(257, 90);
+            this.cbSiniflar.Name = "cbSiniflar";
+            this.cbSiniflar.Size = new System.Drawing.Size(152, 24);
+            this.cbSiniflar.TabIndex = 1;
+            this.cbSiniflar.SelectedIndexChanged += new System.EventHandler(this.cbSiniflar_SelectedIndexChanged);
+            // 
+            // lvOgrenciler
+            // 
+            this.lvOgrenciler.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.lvOgrenciler.Location = new System.Drawing.Point(73, 141);
+            this.lvOgrenciler.Name = "lvOgrenciler";
+            this.lvOgrenciler.Size = new System.Drawing.Size(575, 216);
+            this.lvOgrenciler.TabIndex = 2;
+            this.lvOgrenciler.UseCompatibleStateImageBehavior = false;
+            this.lvOgrenciler.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Öğrenci Adı";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Soyadı";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Telefon";
+            this.columnHeader3.Width = 90;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Adres";
+            this.columnHeader4.Width = 120;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Sınıfı";
+            this.columnHeader5.Width = 80;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(713, 464);
+            this.Controls.Add(this.lvOgrenciler);
+            this.Controls.Add(this.cbSiniflar);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "ADO.Net Tekrar";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbSiniflar;
+        private System.Windows.Forms.ListView lvOgrenciler;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+    }
+}
+
---
+++ wfOKULAdoNet/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wfOKULAdoNet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection("Data Source=WISSEN22\\MSSQLYAZ8; Initial Catalog=OKUL; uid=sa; pwd=123458");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SiniflariGetir();
+        }
+        private void SiniflariGetir()
+        {
+            SqlCommand comm = new SqlCommand("Select SinifAd from Siniflar", conn);
+            if (conn.State == ConnectionState.Closed) conn.Open();
+            SqlDataReader dr;
+            try
+            {
+                dr = comm.ExecuteReader();
+                while (dr.Read())
+                {
+                    cbSiniflar.Items.Add(dr["SinifAd"].ToString());
+                }
+                dr.Close();
+            }
+            catch (SqlException ex)
+            {
+                string hata = ex.Message;
+            }
+            finally { conn.Close(); }
+        }
+
+        private void cbSiniflar_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            OgrencileriGetirBySinif();
+        }
+        private void OgrencileriGetirBySinif()
+        {
+            lvOgrenciler.Items.Clear();
+            SqlCommand comm = new SqlCommand("Select Ad, Soyad, Telefon, Adres, SinifAd, Ogrenciler.ID from Ogrenciler inner join Siniflar on Ogrenciler.sinifID=Siniflar.ID where SinifAd=@SinifAd", conn);
+            comm.Parameters.Add("@SinifAd", SqlDbType.VarChar).Value = cbSiniflar.SelectedItem.ToString();
+            if (conn.State == ConnectionState.Closed) conn.Open();
+            SqlDataReader dr;
+            try
+            {
+                dr = comm.ExecuteReader();
+                int i = 0;
+                while (dr.Read())
+                {
+                    lvOgrenciler.Items.Add(dr[0].ToString());
+                    lvOgrenciler.Items[i].SubItems.Add(dr[1].ToString());
+                    lvOgrenciler.Items[i].SubItems.Add(dr[2].ToString());
+                    lvOgrenciler.Items[i].SubItems.Add(dr[3].ToString());
+                    lvOgrenciler.Items[i].SubItems.Add(dr[4].ToString());
+                    lvOgrenciler.Items[i].SubItems.Add(dr[5].ToString());
+                    i++;
+                }
+                dr.Close();
+            }
+            catch (SqlException ex)
+            {
+                string hata = ex.Message;
+            }
+            finally { conn.Close(); }
+
+        }
+    }
+}
